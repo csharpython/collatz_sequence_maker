b299380b670a7e7c1c3821bf87e96d180b975f5b
csharpython <121863991+csharpython@users.noreply.github.com>
2023-01-15 18:27:31 +0900

    file upload

---
+++ coll2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+using namespace std;
+int main(){
+	long long a,b,k;
+	scanf("%lld", &a);
+	scanf("%lld", &b);
+	for(int i=a;i<=b;i++){
+		k=i;
+		printf("%lld", k);
+		while(k!=1){
+			if(k%2==0)k/=2;
+			else k=k*3+1;
+			printf(" %lld",k);
+		}
+		printf("\n");
+	}
+	return 0;
+}
